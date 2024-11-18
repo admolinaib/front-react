eee4d0b23a8bac3339649471e8c74296a21a00b2
Daniel Bustamante <danielbustamante1606@gmail.com>
2024-11-18 16:06:24 -0500

    dbl feat modificacion de la pagina Categorias y Gastos. Se agrega useForm y useState para conexion con API desde el front

--- src/components/forms/CategoriesForm.jsx
+++ src/components/forms/CategoriesForm.jsx
@@ -1,30 +1,50 @@
 import React from 'react'
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 export default function CategoriesForm() {
+    let {register, handleSubmit, formState : {errors}} = useForm()
+    let onsubmited = async(data)=>{
+        console.log("Datos del formulario");
+        console.log(data);
+        try {
+            let respuesta = await axios.post("http://localhost:8000/categorias", data)
+            console.log("Respuesta del servidor");
+            console.log(respuesta);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+    const [inputValue, setInputValue] = useState("")
+    const handleSelect = (text) => {
+    setInputValue(text)
+    }
   return (
     <>
         <div class="d-flex justify-content-center align-items-center min-vh-100">
-            <form class="d-flex flex-column align-items-center w-50 p-3 rounded shadow-sm bg-purple-transparent">
+            <form onSubmit={handleSubmit(onsubmited)} class="d-flex flex-column align-items-center w-50 p-3 rounded shadow-sm bg-purple-transparent">
                 <h2>Registra tu Categoría</h2>
                 <div class="mb-3 w-75 p-3 d-flex align-items-center input-group position-relative">
                     <i class="bx bx-category"></i>
-                    <input type="text" class="form-control shadow-sm ms-2" id="category" placeholder="Nombre de categoria:" required/>
+                    <input type="text" {...register("nombreCategoria", {required: true})} class="form-control shadow-sm ms-2" value={inputValue} onChange={(e)=> setInputValue(e.target.value)} id="category" placeholder="Nombre de categoria:" required/>
                     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Seleccionar</button>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><hr class="dropdown-divider"/></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            <li><button onClick={()=> handleSelect("Transporte")} className="dropdown-item" >Transporte</button></li>
+                            <li><button onClick={()=> handleSelect("Alimentación")} className="dropdown-item" >Alimentación</button></li>
+                            <li><button onClick={()=> handleSelect("Moda")} className="dropdown-item" >Moda</button></li>
+                            <li><button onClick={()=> handleSelect("Salud y bienestar")} className="dropdown-item" >Salud y bienestar</button></li>
+                            <li><button onClick={()=> handleSelect("Hogar")} className="dropdown-item" >Hogar</button></li>
                         </ul>
                 </div>                 
                 <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
                     <i class="bx bx-notepad"></i>
-                    <input type="text" class="form-control shadow-sm ms-2" id="descripcion" placeholder="Descripción:" required/>
+                    <input type="text" {...register("descripcion", {required: true})} class="form-control shadow-sm ms-2" id="descripcion" placeholder="Descripción:" required/>
                 </div>
                 <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
                     <i class="bx bx-image"></i>
-                    <input type="file" class="form-control shadow-sm ms-2" id="full-name" placeholder="Foto:"/>
+                    <input type="file" {...register("fotoCategoria", {required: true})} class="form-control shadow-sm ms-2" id="full-name" placeholder="Foto:"/>
                 </div>   
                 <button type="submit" class="btn btn-primary mb-3">Enviar</button>
             </form>
--- src/components/forms/ExpensesForm.jsx
+++ src/components/forms/ExpensesForm.jsx
@@ -1,29 +1,63 @@
 import React from 'react'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+
+
 
 export default function ExpensesForm() {
-  return (
+    let {register, handleSubmit, formState : {errors}} = useForm()
+    let onsubmited = async(data)=>{
+        console.log("Datos del formulario");
+        console.log(data);
+        try {
+            let respuesta = await axios.post("http://localhost:8000/gastos", data)
+            console.log("Respuesta del servidor");
+            console.log(respuesta);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+    const [inputValue, setInputValue] = useState("")
+    const handleSelect = (text) => {
+    setInputValue(text)
+    }
+    return (
     <>
-        <div class="d-flex justify-content-center align-items-center min-vh-100">
-            <form class="d-flex flex-column align-items-center w-50 p-3 rounded shadow-sm bg-purple-transparent">
+        <div className="d-flex justify-content-center align-items-center min-vh-100">
+            <form onSubmit={handleSubmit(onsubmited)} className="d-flex flex-column align-items-center w-50 p-3 rounded shadow-sm bg-purple-transparent">
                 <h2>Registra tus Gastos</h2>
                 
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-notepad"></i>
-                    <input type="text" class="form-control shadow-sm ms-2" id="descripcion" placeholder="Descripción" required/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-notepad"></i>
+                    <input type="text" {...register("descripcion", {required: true})} className="form-control shadow-sm ms-2" id="descripcion" placeholder="Descripción" required/>
+                    {errors.descripcion && <p>Ingresa una descripción.</p>}
                 </div>
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-category"></i>
-                    <input type="number" class="form-control shadow-sm ms-2" id="valor" placeholder="Categoría" required/>
+                <div className="input-group mb-3 d-flex w-50 ">
+                    <i className="bx bx-category"></i>
+                    <input type="text" {...register("categoria", {required: true})} className="form-control rounded " value={inputValue} onChange={(e)=> setInputValue(e.target.value)} aria-label="Text input with dropdown button" placeholder="Categoría"/>
+                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Elige Categoría</button>
+                    <ul className="dropdown-menu dropdown-menu-end">
+                        <li><button onClick={()=> handleSelect("Transporte")} className="dropdown-item" >Transporte</button></li>
+                        <li><button onClick={()=> handleSelect("Alimentación")} className="dropdown-item" >Alimentación</button></li>
+                        <li><button onClick={()=> handleSelect("Moda")} className="dropdown-item" >Moda</button></li>
+                        <li><button onClick={()=> handleSelect("Salud y bienestar")} className="dropdown-item" >Salud y bienestar</button></li>
+                        <li><button onClick={()=> handleSelect("Hogar")} className="dropdown-item" >Hogar</button></li>
+                    </ul>
                 </div>
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-bar-chart-alt-2"></i>
-                    <input type="number" class="form-control shadow-sm ms-2" id="valor" placeholder="Valor" required/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-bar-chart-alt-2"></i>
+                    <input type="number" {...register("valor", {required: true})} className="form-control shadow-sm ms-2" id="valor" placeholder="Valor" required/>
+                    {errors.valor && <p>Digita un valor valido.</p>}
                 </div>
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-calendar"></i>
-                    <input type="date" class="form-control shadow-sm ms-2" id="fecha" placeholder="Fecha de registro de ingreso" required/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-calendar"></i>
+                    <input type="date" {...register("fecha", {required: true})} className="form-control shadow-sm ms-2" id="fecha" placeholder="Fecha de registro de ingreso" required/>
+                    {errors.fecha && <p>Ingresa una fecha valida.</p>}
                 </div>
-                <button type="submit" class="btn btn-primary mb-3">Emnviar</button>
+                <button type="submit" className="btn btn-primary mb-3">Registrar</button>
             </form>
         </div>
     </>

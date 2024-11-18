91915e6365451e81fe2292fd989190695d0a4074
Daniela Molina <admolinaib@cesde.net>
2024-11-18 15:15:39 -0500

    dmi feat corrección de clases

--- src/components/forms/CategoriesForm.jsx
+++ src/components/forms/CategoriesForm.jsx
@@ -3,30 +3,30 @@ import React from 'react'
 export default function CategoriesForm() {
   return (
     <>
-        <div class="d-flex justify-content-center align-items-center min-vh-100">
-            <form class="d-flex flex-column align-items-center w-50 p-3 rounded shadow-sm bg-purple-transparent">
+        <div className="d-flex justify-content-center align-items-center min-vh-100">
+            <form className="d-flex flex-column align-items-center w-50 p-3 rounded shadow-sm bg-purple-transparent">
                 <h2>Registra tu Categoría</h2>
-                <div class="mb-3 w-75 p-3 d-flex align-items-center input-group position-relative">
-                    <i class="bx bx-category"></i>
-                    <input type="text" class="form-control shadow-sm ms-2" id="category" placeholder="Nombre de categoria:" required/>
-                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Seleccionar</button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><hr class="dropdown-divider"/></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center input-group position-relative">
+                    <i className="bx bx-category"></i>
+                    <input type="text" className="form-control shadow-sm ms-2" id="category" placeholder="Nombre de categoria:" required/>
+                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Seleccionar</button>
+                        <ul className="dropdown-menu dropdown-menu-end">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            <li><hr className="dropdown-divider"/></li>
+                            <li><a className="dropdown-item" href="#">Separated link</a></li>
                         </ul>
                 </div>                 
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-notepad"></i>
-                    <input type="text" class="form-control shadow-sm ms-2" id="descripcion" placeholder="Descripción:" required/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-notepad"></i>
+                    <input type="text" className="form-control shadow-sm ms-2" id="descripcion" placeholder="Descripción:" required/>
                 </div>
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-image"></i>
-                    <input type="file" class="form-control shadow-sm ms-2" id="full-name" placeholder="Foto:"/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-image"></i>
+                    <input type="file" className="form-control shadow-sm ms-2" id="full-name" placeholder="Foto:"/>
                 </div>   
-                <button type="submit" class="btn btn-primary mb-3">Enviar</button>
+                <button type="submit" className="btn btn-primary mb-3">Enviar</button>
             </form>
         </div>
     </>
--- src/components/forms/ExpensesForm.jsx
+++ src/components/forms/ExpensesForm.jsx
@@ -3,27 +3,27 @@ import React from 'react'
 export default function ExpensesForm() {
   return (
     <>
-        <div class="d-flex justify-content-center align-items-center min-vh-100">
-            <form class="d-flex flex-column align-items-center w-50 p-3 rounded shadow-sm bg-purple-transparent">
+        <div className="d-flex justify-content-center align-items-center min-vh-100">
+            <form className="d-flex flex-column align-items-center w-50 p-3 rounded shadow-sm bg-purple-transparent">
                 <h2>Registra tus Gastos</h2>
                 
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-notepad"></i>
-                    <input type="text" class="form-control shadow-sm ms-2" id="descripcion" placeholder="Descripción" required/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-notepad"></i>
+                    <input type="text" className="form-control shadow-sm ms-2" id="descripcion" placeholder="Descripción" required/>
                 </div>
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-category"></i>
-                    <input type="number" class="form-control shadow-sm ms-2" id="valor" placeholder="Categoría" required/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-category"></i>
+                    <input type="number" className="form-control shadow-sm ms-2" id="valor" placeholder="Categoría" required/>
                 </div>
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-bar-chart-alt-2"></i>
-                    <input type="number" class="form-control shadow-sm ms-2" id="valor" placeholder="Valor" required/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-bar-chart-alt-2"></i>
+                    <input type="number" className="form-control shadow-sm ms-2" id="valor" placeholder="Valor" required/>
                 </div>
-                <div class="mb-3 w-75 p-3 d-flex align-items-center position-relative">
-                    <i class="bx bx-calendar"></i>
-                    <input type="date" class="form-control shadow-sm ms-2" id="fecha" placeholder="Fecha de registro de ingreso" required/>
+                <div className="mb-3 w-75 p-3 d-flex align-items-center position-relative">
+                    <i className="bx bx-calendar"></i>
+                    <input type="date" className="form-control shadow-sm ms-2" id="fecha" placeholder="Fecha de registro de ingreso" required/>
                 </div>
-                <button type="submit" class="btn btn-primary mb-3">Emnviar</button>
+                <button type="submit" className="btn btn-primary mb-3">Emnviar</button>
             </form>
         </div>
     </>
